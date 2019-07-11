3d17dc673ebb1e8cf4f839d4a9a774c81d4546e4
asadutzaman <mdasadutzaman@gmail.com>
2019-07-11 15:52:26 +0600

    form complete

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -3,7 +3,71 @@
 
 <div class="page-content-wrapper">
     <div class="container-fluid">
-        
+        <div class="row">
+            <div class="col-6">
+                <div class="card m-b-20">
+                    <div class="card-body">
+
+                        <h4 class="mt-0 header-title">Form</h4>
+                        <p class="text-muted m-b-30 font-14">form for example</p>
+                        <!-- name -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Name:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="name" id='name' name='name'/>
+                            </div>
+                        </div>
+                        <!-- contact_person -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Contact Person:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="contact_person" id='contact_person' name='contact_person'/>
+                            </div>
+                        </div>
+                        <!-- contact_number -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Contact number:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="contact_number" id='contact_number' name='contact_number'/>
+                            </div>
+                        </div>
+                        <!-- alt_contact_person -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Alt Contact Person:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="alt_contact_person" id='alt_contact_person' name='alt_contact_person'/>
+                            </div>
+                        </div>
+                        <!-- supplier_email -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Email:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="supplier_email" id='supplier_email' name='supplier_email'/>
+                            </div>
+                        </div>
+                        <!-- supplier_address -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Name:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="supplier_address" id='supplier_address' name='supplier_address'/>
+                            </div>
+                        </div>
+                        <!-- extra_info -->
+                        <div class="form-group row">
+                            <label for="example-text-input" class="col-sm-2 col-form-label">Extra Info:</label>
+                            <div class="col-sm-10">
+                                <input class="form-control autocomplete_txt" type='text' data-type="extra_info" id='extra_info' name='extra_info'/>
+                            </div>
+                        </div>
+                        <!-- Button -->
+                        <div class="form-group row">
+                            <button >Submit </button>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
     </div><!-- container -->
 </div> <!-- Page content Wrapper -->
 
