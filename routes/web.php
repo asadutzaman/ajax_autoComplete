714d34cd96d2d6e276bcca751647e0884fe64528
asadutzaman <mdasadutzaman@gmail.com>
2019-07-11 16:31:04 +0600

    define route

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('autocomplete', 'AjaxAutocompleteController@index');
+Route::get('searchajax', ['as'=>'searchajax','uses'=>'AjaxAutocompleteController@searchResponse']);
