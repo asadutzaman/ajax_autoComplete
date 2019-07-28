e8a45e9c24d3f8d9b771ea4885f8403e43ac9795
asadutzaman <mdasadutzaman@gmail.com>
2019-07-28 13:32:43 +0600

    filter total_item improved

--- app/Http/Controllers/AjaxAutocompleteController.php
+++ app/Http/Controllers/AjaxAutocompleteController.php
@@ -194,18 +194,18 @@ class AjaxAutocompleteController extends Controller
         }
         // total_item
         else if($request->type=='total_item'){
-            $inventories=DB::Table('inventories')->select('total_item');
+            $inventories=DB::Table('inventories')->select('total_item')->distinct();
             $inventories->where('total_item','LIKE','%'.$query.'%');
             $inventories->limit(10);
 
-            $inventories=$inventories->get();        
+            $inventories=$inventories->get();
             $data=array();
             foreach ($inventories as $inventorie) {
                 $data[]=array('total_item'=>$inventorie->total_item);
             }
             if(count($data)){
                 return $data;
-            }    
+            }
             else {
                 return "No data found";
             }
